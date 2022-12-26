1e50d1cb1180307e98b48b9a076c4c48658db1f5
KrissKaron <119007921+KrissKaron@users.noreply.github.com>
2022-12-26 15:21:06 +0200

    Create recognition.py

---
+++ recognition.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from keras import datasets, layers, models
+import tensorflow as tf
+import cv2 as cv
+
+(training_imgs, training_labels), (testing_imgs, testing_labels) = datasets.
